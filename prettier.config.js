32fa7b6a3d736eb42ef8721e699e3c31d6a2d0d8
Gilles Lima <gillestoim@gmail.com>
2021-04-23 23:03:18 -0300

    chore: configure project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 80,
+  singleQuote: true,
+  semi: false,
+  useTabs: false,
+  trailingComma: 'none',
+  tabWidth: 2
+}
