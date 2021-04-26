bd174467b3109abefb0c552375de34f2be0697ff
Gilles Lima <gillestoim@gmail.com>
2021-04-26 00:38:10 -0300

    styles: update summary wrapper

--- src/styles/elements/SummaryWrapper.ts
+++ src/styles/elements/SummaryWrapper.ts
@@ -1,16 +1,30 @@
 import styled from 'styled-components'
 
-export const SummaryWrapper = styled.div<{ display?: string }>`
+export const SummaryWrapper = styled.div`
   position: absolute;
-  display: ${props => props.display || 'hidden'};
+  display: block;
 
-  top: 40px;
-  right: 5px;
+  top: -10px;
+  right: -10px;
+  left: -10px;
+
   z-index: 9;
 
-  background-color: #FFF;
-  border-radius: 14px;
-  padding: 10px;
+  background-color: #fff;
+  border-radius: 0;
+  padding: 40px 0 20px 0;
 
   box-shadow: 0 3px 7px 0px #c5c5c5;
+
+  @media (min-width: 300px) and (max-width: 359px) {
+    > div {
+      padding: 0 10px;
+    }
+  }
+
+  @media (min-width: 360px) {
+    > div {
+      padding: 0 20px;
+    }
+  }
 `
